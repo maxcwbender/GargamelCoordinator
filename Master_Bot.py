58e29d2df1d4edf4c7637fc4e5368e4b623d93d4
crowedev <steve@crowe.dev>
2025-07-18 09:57:23 -0400

    Update Master_Bot.py

--- Master_Bot.py
+++ Master_Bot.py
@@ -175,7 +175,7 @@ class Master_Bot(commands.Bot):
 
     async def play_sound(self, interaction: discord.Interaction, channel: discord.VoiceChannel, sound: str):
         SOUNDS = {
-            "start_game": "/root/GargamelCoordinator/sounds/mk64_racestart.wav",
+            "start_game": "/root/GargamelCoordinator/sounds/mk64_racestart_fixed.wav",
             "countdown": "/root/GargamelCoordinator/sounds/mk64_countdown.wav",
         }
 
