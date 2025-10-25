9aecfade301336a79b1d0921ce2610cadc4873e8
crowedev <steve@crowe.dev>
2025-10-25 16:46:47 -0400

    Update DotaTalker.py

--- DotaTalker.py
+++ DotaTalker.py
@@ -321,7 +321,7 @@ class ClientWrapper:
 
     async def alert_game_polling_started(self):
         self.polling_active = True
-        e
+        
         if not self.dota:
             logger.exception(f"Game {self.game_id} has no dota client available")
 
